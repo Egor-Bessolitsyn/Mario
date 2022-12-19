ff81cf426a4ecfb8b22b6a49b3a1bb2cf4dfd369
Egor Bessolitsyn <bessolitsynegor@yandex.ru>
2022-12-19 19:28:02 +0300

    4

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@ import pygame
 import os
 import sys
 
-FPS = 50
+FPS = 100
 size = WIDTH, HEIGHT = 800, 600
 clock = pygame.time.Clock()
 pygame.init()
@@ -26,161 +26,44 @@ def load_image(name, colorkey=None):
     return image
 
 
-def load_level(filename):
-    filename = "data/" + filename
-    # читаем уровень, убирая символы перевода строки
-    with open(filename, 'r') as mapFile:
-        level_map = [line.strip() for line in mapFile]
+class AnimatedSprite(pygame.sprite.Sprite):
+    def __init__(self, sheet, columns, rows, x, y):
+        super().__init__(all_sprites)
+        self.frames = []
+        self.cut_sheet(sheet, columns, rows)
+        self.cur_frame = 0
+        self.image = self.frames[self.cur_frame]
+        self.rect = self.rect.move(x, y)
+        self.count = 0
 
-    # и подсчитываем максимальную длину
-    max_width = max(map(len, level_map))
+    def cut_sheet(self, sheet, columns, rows):
+        self.rect = pygame.Rect(0, 0, sheet.get_width() // columns,
+                                sheet.get_height() // rows)
+        for j in range(rows):
+            for i in range(columns):
+                frame_location = (self.rect.w * i, self.rect.h * j)
+                self.frames.append(sheet.subsurface(pygame.Rect(
+                    frame_location, self.rect.size)))
 
-    # дополняем каждую строку пустыми клетками ('.')
-    return list(map(lambda x: x.ljust(max_width, '.'), level_map))
-
-
-tile_images = {
-    'wall': load_image('box.png'),
-    'empty': load_image('grass.png')
-}
-player_image = load_image('mar.png')
-
-tile_width = tile_height = 50
-
-
-class Tile(pygame.sprite.Sprite):
-    def __init__(self, tile_type, pos_x, pos_y):
-        super().__init__(tiles_group, all_sprites)
-        self.image = tile_images[tile_type]
-        if tile_type == 'wall':
-            self.add(wall_group)
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x, tile_height * pos_y)
-
-
-class Player(pygame.sprite.Sprite):
-    def __init__(self, pos_x, pos_y):
-        super().__init__(player_group, all_sprites)
-        self.image = player_image
-        self.rect = self.image.get_rect().move(
-            tile_width * pos_x + 15, tile_height * pos_y + 5)
-
-    def move(self, dx, dy):
-        self.rect.x += dx * tile_width
-        self.rect.y += dy * tile_height
-        self.update()
-
-
-class Camera:
-    # зададим начальный сдвиг камеры
-    def __init__(self, width, height):
-        self.dx = 0
-        self.dy = 0
-        self.width = width
-        self.height = height
-
-    # сдвинуть объект obj на смещение камеры
-    def apply(self, obj):
-        obj.rect.x += self.dx
-        obj.rect.y += self.dy
-
-    # позиционировать камеру на объекте target
-    def update(self, target):
-        self.dx = -(target.rect.x + target.rect.w // 2 - self.width // 2)
-        self.dy = -(target.rect.y + target.rect.h // 2 - self.height // 2)
-
-
-def terminate():
-    pygame.quit()
-    sys.exit()
-
-
-def start_screen():
-    intro_text = ["ЗАСТАВКА", "",
-                  "Правила игры",
-                  "Если в правилах несколько строк,",
-                  "приходится выводить их построчно"]
-
-    fon = pygame.transform.scale(load_image('fon.jpg'), (WIDTH, HEIGHT))
-    screen.blit(fon, (0, 0))
-    font = pygame.font.Font(None, 30)
-    text_coord = 50
-    for line in intro_text:
-        string_rendered = font.render(line, 1, pygame.Color('white'))
-        intro_rect = string_rendered.get_rect()
-        text_coord += 10
-        intro_rect.top = text_coord
-        intro_rect.x = 10
-        text_coord += intro_rect.height
-        screen.blit(string_rendered, intro_rect)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                terminate()
-            elif event.type == pygame.KEYDOWN or \
-                    event.type == pygame.MOUSEBUTTONDOWN:
-                return  # начинаем игру
-        pygame.display.flip()
-        clock.tick(FPS)
-
-
-def generate_level(level):
-    new_player, x, y = None, None, None
-    for y in range(len(level)):
-        for x in range(len(level[y])):
-            if level[y][x] == '.':
-                Tile('empty', x, y)
-            elif level[y][x] == '#':
-                Tile('wall', x, y)
-            elif level[y][x] == '@':
-                Tile('empty', x, y)
-                new_player = Player(x, y)
-    # вернем игрока, а также размер поля в клетках
-    return new_player, x, y
+    def update(self):
+        self.count = (self.count + 1) % 10
+        if self.count == 0:
+            self.cur_frame = (self.cur_frame + 1) % len(self.frames)
+            self.image = self.frames[self.cur_frame]
 
 
 if __name__ == '__main__':
-
     all_sprites = pygame.sprite.Group()
-    tiles_group = pygame.sprite.Group()
-    wall_group = pygame.sprite.Group()
-    player_group = pygame.sprite.Group()
-
-    start_screen()
-    player, level_x, level_y = generate_level(load_level('level01.txt'))
-    size = (level_x + 1) * tile_width, (level_y + 1) * tile_height
-    screen = pygame.display.set_mode(size)
-    camera = Camera(*size)
+    dragon = AnimatedSprite(load_image("dragon_sheet8x2.png"), 8, 2, 350, 250)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT:
-                    player.move(-1, 0)
-                    if pygame.sprite.spritecollideany(player, wall_group):
-                        player.move(1, 0)
-                if event.key == pygame.K_RIGHT:
-                    player.move(1, 0)
-                    if pygame.sprite.spritecollideany(player, wall_group):
-                        player.move(-1, 0)
-                if event.key == pygame.K_DOWN:
-                    player.move(0, 1)
-                    if pygame.sprite.spritecollideany(player, wall_group):
-                        player.move(0, -1)
-                if event.key == pygame.K_UP:
-                    player.move(0, -1)
-                    if pygame.sprite.spritecollideany(player, wall_group):
-                        player.move(0, 1)
+
+        screen.fill((0, 0, 0))
         all_sprites.update()
-        screen.fill((255, 255, 255))
-        camera.update(player)
-        for sprite in all_sprites:
-            camera.apply(sprite)
         all_sprites.draw(screen)
-        player_group.draw(screen)
         pygame.display.flip()
         clock.tick(FPS)
     pygame.quit()
---
+++ stars.py
@@ -0,0 +1,91 @@
+import pygame
+import os
+import sys
+import random
+
+FPS = 50
+size = WIDTH, HEIGHT = 800, 600
+clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode(size)
+GRAVITY = 0.5
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    if colorkey is not None:
+        image = image.convert()
+        if colorkey == -1:
+            colorkey = image.get_at((0, 0))
+        image.set_colorkey(colorkey)
+    else:
+        image = image.convert_alpha()
+    return image
+
+
+screen_rect = (0, 0, WIDTH, HEIGHT)
+
+
+class Particle(pygame.sprite.Sprite):
+    # сгенерируем частицы разного размера
+    fire = [load_image("star.png")]
+    for scale in (5, 10, 20):
+        fire.append(pygame.transform.scale(fire[0], (scale, scale)))
+
+    def __init__(self, pos, dx, dy):
+        super().__init__(all_sprites)
+        self.image = random.choice(self.fire)
+        self.rect = self.image.get_rect()
+
+        # у каждой частицы своя скорость — это вектор
+        self.velocity = [dx, dy]
+        # и свои координаты
+        self.rect.x, self.rect.y = pos
+
+        # гравитация будет одинаковой (значение константы)
+        self.gravity = GRAVITY
+
+    def update(self):
+        # применяем гравитационный эффект:
+        # движение с ускорением под действием гравитации
+        self.velocity[1] += self.gravity
+        # перемещаем частицу
+        self.rect.x += self.velocity[0]
+        self.rect.y += self.velocity[1]
+        # убиваем, если частица ушла за экран
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+
+def create_particles(position):
+    # количество создаваемых частиц
+    particle_count = 200
+    # возможные скорости
+    numbers_x = range(-5, 6)
+    numbers_y = range(-20, 6)
+    for _ in range(particle_count):
+        Particle(position, random.choice(numbers_x), random.choice(numbers_y))
+
+
+if __name__ == '__main__':
+    all_sprites = pygame.sprite.Group()
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                # создаём частицы по щелчку мыши
+                create_particles(pygame.mouse.get_pos())
+
+        screen.fill((0, 0, 0))
+        all_sprites.update()
+        all_sprites.draw(screen)
+        pygame.display.flip()
+        clock.tick(FPS)
+    pygame.quit()
